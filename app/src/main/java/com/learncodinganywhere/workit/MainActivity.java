1940dc889a0bf1aa52bfb119cdb7dbab116b963c
Kevin <103957894+kevlarsov@users.noreply.github.com>
2022-07-19 14:04:58 -0600

    Story 2 base layout view v1

--- app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
+++ app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
@@ -1,6 +1,10 @@
 package com.learncodinganywhere.workit;
 
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
 
