7a0be28c6cef5ac5955350ada93919f234b2f436
kevlarsov <103957894+kevlarsov@users.noreply.github.com>
2022-07-29 08:33:43 -0600

    db link, v2

---
+++ app/src/main/java/com/learncodinganywhere/workit/BackgroundWorker.java
@@ -0,0 +1,106 @@
+package com.learncodinganywhere.workit;
+
+import android.content.Context;
+import android.os.AsyncTask;
+
+import androidx.appcompat.app.AlertDialog;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class BackgroundWorker extends AsyncTask<String, Void, String> {
+    Context context;
+    AlertDialog alertDialog;
+
+    BackgroundWorker(Context ctx) {
+        context = ctx;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+        String type = params[0];
+        String save_url = "http://192.168.1.94/dbconfig.php";
+        if (type.equals("save")) {
+            try {
+                String fname = params[1];
+                String lname = params[2];
+                String address1 = params[3];
+                String address2 = params[4];
+                String state = params[5];
+                String zip = params[6];
+                String email = params[7];
+                String phone = params[8];
+                String efname = params[9];
+                String elname = params[10];
+                String ephone = params[11];
+                String erelationship = params[12];
+                URL url = new URL(save_url);
+                HttpURLConnection httpURLConnection = (HttpURLConnection)url.openConnection();
+                httpURLConnection.setRequestMethod("POST");
+                httpURLConnection.setDoOutput(true);
+                httpURLConnection.setDoInput(true);
+                OutputStream outputStream = httpURLConnection.getOutputStream();
+                BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(outputStream, "UTF-8"));
+                String post_data =
+                        URLEncoder.encode("fname", "UTF-8") + "=" + URLEncoder.encode(fname, "UTF-8") + "&"
+                        + URLEncoder.encode("lname", "UTF-8") + "=" + URLEncoder.encode(lname, "UTF-8") + "&"
+                        + URLEncoder.encode("address1", "UTF-8") + "=" + URLEncoder.encode(address1, "UTF-8") + "&"
+                        + URLEncoder.encode("address2", "UTF-8") + "=" + URLEncoder.encode(address2, "UTF-8") + "&"
+                        + URLEncoder.encode("state", "UTF-8") + "=" + URLEncoder.encode(state, "UTF-8") + "&"
+                        + URLEncoder.encode("zip", "UTF-8") + "=" + URLEncoder.encode(zip, "UTF-8") + "&"
+                        + URLEncoder.encode("email", "UTF-8") + "=" + URLEncoder.encode(email, "UTF-8") + "&"
+                        + URLEncoder.encode("phone", "UTF-8") + "=" + URLEncoder.encode(phone, "UTF-8") + "&"
+                        + URLEncoder.encode("efname", "UTF-8") + "=" + URLEncoder.encode(efname, "UTF-8") + "&"
+                        + URLEncoder.encode("elname", "UTF-8") + "=" + URLEncoder.encode(elname, "UTF-8") + "&"
+                        + URLEncoder.encode("ephone", "UTF-8") + "=" + URLEncoder.encode(ephone, "UTF-8") + "&"
+                        + URLEncoder.encode("erelationship", "UTF-8") + "=" + URLEncoder.encode(erelationship, "UTF-8");
+                bufferedWriter.write(post_data);
+                bufferedWriter.flush();
+                bufferedWriter.close();
+                outputStream.close();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, "iso-8859-1"));
+                String result = "";
+                String line = "";
+                while ((line = bufferedReader.readLine()) != null) {
+                    result += line;
+                }
+                bufferedReader.close();
+                inputStream.close();
+                httpURLConnection.disconnect();
+                return result;
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
+    }
+
+    @Override
+    protected void onPreExecute() {
+        alertDialog = new AlertDialog.Builder(context).create();
+        alertDialog.setTitle("Login Status");
+    }
+
+    @Override
+    protected void onPostExecute(String result) {
+        alertDialog.setMessage(result);
+        alertDialog.show();
+    }
+
+    @Override
+    protected void onProgressUpdate(Void... values) {
+        super.onProgressUpdate(values);
+    }
+}
--- app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
+++ app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
@@ -17,16 +17,54 @@ import android.widget.ListView;
 import android.widget.PopupMenu;
 
 import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.textfield.TextInputEditText;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private DrawerLayout drawer;
+    Button Button;
+    TextInputEditText Fname, Lname, Address1, Address2, State, Zip, Email, Phone, Efname, Elname, Ephone, Erelationship;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button = findViewById(R.id.button);
+        Fname = findViewById(R.id.first_field);
+        Lname = findViewById(R.id.last_field);
+        Address1 = findViewById(R.id.address_field1);
+        Address2 = findViewById(R.id.address_field2);
+        State = findViewById(R.id.state_field);
+        Zip = findViewById(R.id.zip_field);
+        Email = findViewById(R.id.email_field);
+        Phone = findViewById(R.id.phone_field);
+        Efname = findViewById(R.id.efirst_field);
+        Elname = findViewById(R.id.elast_field);
+        Ephone = findViewById(R.id.ephone_field);
+        Erelationship = findViewById(R.id.erelationship_field);
     }
+
+    public void onClick(View view){
+        String fname = Fname.getText().toString();
+        String lname = Lname.getText().toString();
+        String address1 = Address1.getText().toString();
+        String address2 = Address2.getText().toString();
+        String state = State.getText().toString();
+        String zip = Zip.getText().toString();
+        String email = Email.getText().toString();
+        String phone = Phone.getText().toString();
+        String efname = Efname.getText().toString();
+        String elname = Elname.getText().toString();
+        String ephone = Ephone.getText().toString();
+        String erelationship = Erelationship.getText().toString();
+
+        BackgroundWorker backgroundWorker = new BackgroundWorker(this);
+        backgroundWorker.execute(fname, lname, address1, address2, state, zip, email, phone, efname, elname, ephone, erelationship);
+    }
+
 
     public void baseLayout(View loginButton) {
         setContentView(R.layout.activity_base);
