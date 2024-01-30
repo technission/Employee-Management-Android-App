81534391d9aac2f4f1257c7e8b35deb65493569e
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2024-01-30 15:40:00 -0800

    fix broken activity function

--- app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
+++ app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
@@ -8,9 +8,10 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuItemClickListener {
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,44 +23,14 @@ public class MainActivity extends AppCompatActivity implements PopupMenu.OnMenuI
     }
 
     public void showPopUp(View v) {
-        PopupMenu popup = new PopupMenu(this, v);
-        popup.setOnMenuItemClickListener(this);
-        popup.inflate(R.menu.popup_menu);
-        popup.show();
+        setContentView(R.layout.menu_item_popout);
     }
 
-    @SuppressLint("NonConstantResourceId")
-    @Override
-    public boolean onMenuItemClick(MenuItem menuItem) {
-        switch (menuItem.getItemId()) {
-            case R.id.schedule:
-                Toast.makeText(this, "Item 1 Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.messenger:
-                Toast.makeText(this, "Item 2 Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.pay:
-                Toast.makeText(this, "Item 3 Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.paymentInfo:
-                Toast.makeText(this, "Item 4 Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.employee_info:
-                Toast.makeText(this, "Item 5 Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.profile:
-                Toast.makeText(this, "Item 6 Clicked", Toast.LENGTH_SHORT).show();
-                return true;
-            default:
-                return false;
-        }
+    public void closePopUp(View v) {
+        setContentView(R.layout.activity_landing_page);
     }
 
-    public void EmployeeInfo(MenuItem item) {
-        int id = item.getItemId();
-
-        if (id == R.id.employee_info) {
-            setContentView(R.layout.activity_emp_info);
-        }
+    public void EmployeeInfo(View v) {
+        setContentView(R.layout.activity_emp_info);
     }
 }
