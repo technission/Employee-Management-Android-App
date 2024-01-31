66eb5e1e25ad8ed288f8847cd32319c176cb806d
Alexander Sanderson <67873187+Vexelior@users.noreply.github.com>
2024-01-30 18:31:22 -0800

    start of mysql database connection

--- app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
+++ app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
@@ -1,15 +1,17 @@
 package com.learncodinganywhere.workit;
-
 import androidx.appcompat.app.AppCompatActivity;
-
-import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.PopupMenu;
-import android.widget.TextView;
-import android.widget.Toast;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,8 +20,57 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        try {
+            new NetworkTask().execute("https://172.20.224.1/workit/connect.php");
+        } catch (Exception ex) {
+            System.out.println(ex.getMessage());
+        }
+
+
         Button btn = (findViewById(R.id.loginButton));
         btn.setOnClickListener(v -> setContentView(R.layout.activity_landing_page));
+    }
+
+    private class NetworkTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... params) {
+            // Perform network operations in the background
+            System.out.println("Attempting to connect to WAMP MySQL...");
+            try {
+                System.out.println(fetchDataFromNetwork(params[0]));
+                return fetchDataFromNetwork(params[0]);
+            } catch (Exception ex) {
+                System.out.println(ex.getMessage());
+            }
+
+            return null;
+        }
+
+
+        @Override
+        protected void onPostExecute(String result) {
+            System.out.println(result);
+        }
+    }
+
+    private String fetchDataFromNetwork(String urlString) throws IOException {
+        URL url = new URL(urlString);
+        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+        try {
+            InputStream in = urlConnection.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+            StringBuilder stringBuilder = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                stringBuilder.append(line).append("\n");
+            }
+            System.out.println("Stringbuilder: " + stringBuilder);
+
+            return stringBuilder.toString();
+        } finally {
+            urlConnection.disconnect();
+        }
     }
 
     public void showPopUp(View v) {
