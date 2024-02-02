9ba0e68ac49b412673c350a2cae4bfe85b5810ae
Alexander Sanderson <asanderson1994s@gmail.com>
2024-02-02 12:57:00 -0800

    firebase database

--- app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
+++ app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
@@ -1,17 +1,17 @@
 package com.learncodinganywhere.workit;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.net.HttpURLConnection;
-import java.net.URL;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,57 +20,34 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        try {
-            new NetworkTask().execute("https://172.20.224.1/workit/connect.php");
-        } catch (Exception ex) {
-            System.out.println(ex.getMessage());
-        }
+        final String TAG = "Firebase";
 
+        // Write a message to the database
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+
+        myRef.setValue("Hello, World!");
+
+        // Read from the database
+        // Read from the database
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                String value = dataSnapshot.getValue(String.class);
+                Log.d(TAG, "Value is: " + value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Failed to read value
+                Log.w(TAG, "Failed to read value.", error.toException());
+            }
+        });
 
         Button btn = (findViewById(R.id.loginButton));
         btn.setOnClickListener(v -> setContentView(R.layout.activity_landing_page));
-    }
-
-    private class NetworkTask extends AsyncTask<String, Void, String> {
-
-        @Override
-        protected String doInBackground(String... params) {
-            // Perform network operations in the background
-            System.out.println("Attempting to connect to WAMP MySQL...");
-            try {
-                System.out.println(fetchDataFromNetwork(params[0]));
-                return fetchDataFromNetwork(params[0]);
-            } catch (Exception ex) {
-                System.out.println(ex.getMessage());
-            }
-
-            return null;
-        }
-
-
-        @Override
-        protected void onPostExecute(String result) {
-            System.out.println(result);
-        }
-    }
-
-    private String fetchDataFromNetwork(String urlString) throws IOException {
-        URL url = new URL(urlString);
-        HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
-        try {
-            InputStream in = urlConnection.getInputStream();
-            BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-            StringBuilder stringBuilder = new StringBuilder();
-            String line;
-            while ((line = reader.readLine()) != null) {
-                stringBuilder.append(line).append("\n");
-            }
-            System.out.println("Stringbuilder: " + stringBuilder);
-
-            return stringBuilder.toString();
-        } finally {
-            urlConnection.disconnect();
-        }
     }
 
     public void showPopUp(View v) {
