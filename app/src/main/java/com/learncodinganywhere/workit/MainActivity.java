4ee2b3a542b9887017cd15d03784cc86c5e6d5b6
Alexander Sanderson <asanderson1994s@gmail.com>
2024-02-05 13:21:29 -0800

    reset project

--- app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
+++ app/src/main/java/com/learncodinganywhere/workit/MainActivity.java
@@ -1,83 +1,13 @@
 package com.learncodinganywhere.workit;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import androidx.core.view.GravityCompat;
-import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentTransaction;
 import android.os.Bundle;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.Button;
-import android.widget.ListView;
-import android.widget.PopupMenu;
 
-import com.google.android.material.navigation.NavigationView;
-
-public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
-
-    private DrawerLayout drawer;
+public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public void baseLayout(View loginButton) {
-        setContentView(R.layout.activity_base);
-
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.nav_shiftCalender:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new ShiftCalender()).commit();
-                break;
-            case R.id.nav_schedule:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Schedule()).commit();
-                break;
-            case R.id.nav_messenger:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Messenger()).commit();
-                break;
-            case R.id.nav_hoursPay:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new HoursPay()).commit();
-                break;
-            case R.id.nav_paymentInfo:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new PaymentInfo()).commit();
-                break;
-            case R.id.nav_employeeInfo:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new EmployeeInfo()).commit();
-                break;
-            case R.id.nav_profile:
-                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, new Profile()).commit();
-                break;
-        }
-        drawer.closeDrawer(GravityCompat.START);
-        return true;
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (drawer.isDrawerOpen(GravityCompat.START)) {
-            drawer.closeDrawer(GravityCompat.START);
-        } else {
-            super.onBackPressed();
-        }
     }
 }
